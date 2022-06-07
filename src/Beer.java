76f66d21b9629f4b5a60a15e46990222fe058d9e
Alex Shanhin <designport89@gmail.com>
2022-06-08 00:14:28 +0300

    Commit"x";

--- src/Beer.java
+++ src/Beer.java
@@ -5,5 +5,7 @@ public class Beer {
     public static void main(String[] args) {
         System.out.println("Beer for all!!!");
         System.out.println("mbfgx,ffgb");
+        String x = "x";
+
     }
 }
