b19eb7011b9661ec0177e531695235a99e084400
Stepan Belko <Stpn.Belko@gmail.com>
2022-06-08 00:05:37 +0300

    Merge 2

--- src/Beer.java
+++ src/Beer.java
@@ -5,5 +5,6 @@ public class Beer {
     public static void main(String[] args) {
         System.out.println("Beer for all!!!");
         System.out.println("mbfgx,ffgb");
+        System.out.println("Merge 12311");
     }
 }
